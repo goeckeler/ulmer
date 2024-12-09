61617c5e4e6a6b9c58f4704b8b717a01ed1e1b9e
Thorsten Göckeler <thorsten.goeckeler@iteratec.com>
2024-12-10 00:18:29 +0100

    First running prototype with table view

--- src/main/java/com/goeckeler/ulmer/App.java
+++ src/main/java/com/goeckeler/ulmer/App.java
@@ -1,74 +1,72 @@
 package com.goeckeler.ulmer;
 
+import java.time.LocalDate;
+import java.time.Month;
+
+import com.goeckeler.ulmer.object.Gender;
+import com.goeckeler.ulmer.object.Person;
+
 import javafx.application.Application;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-
-
-/**
- * JavaFX App
- */
+/** JavaFX App */
 public class App extends Application {
 
-    @Override
-    public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
+  @Override
+  public void start(Stage stage) {
+    var javaVersion = SystemInfo.javaVersion();
+    var javafxVersion = SystemInfo.javafxVersion();
 
-        var pane = new BorderPane();
-        pane.setPrefSize(640, 480);
-        
-        var label = new Label("JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        pane.setBottom(new StackPane(label));
-        
-        var scene = new Scene(pane);
-        stage.setScene(scene);
-        stage.show();
-        
-        /*
-         * Start of table representation
-         * 
-        TableView<Person> tableView = new TableView();
+    var pane = new BorderPane();
+    pane.setPrefSize(640, 480);
 
-        TableColumn<Person, String> column1 = 
-        new TableColumn<>("First Name");
-        
-        column1.setCellValueFactory(
-            new PropertyValueFactory<>("firstName"));
+    var label = new Label("JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+    pane.setBottom(new StackPane(label));
 
+    TableView<Person> tableView = new TableView<>();
+    tableView.setPlaceholder(new Label("No rows to display."));
 
-        TableColumn<Person, String> column2 = 
-        new TableColumn<>("Last Name");
-        
-        column2.setCellValueFactory(
-            new PropertyValueFactory<>("lastName"));
+    TableColumn<Person, String> nameColumn = new TableColumn<>("Name");
+    nameColumn.setCellValueFactory(
+        person -> new ReadOnlyObjectWrapper<String>(person.getValue().name()));
 
+    TableColumn<Person, String> mailColumn = new TableColumn<>("Mail");
+    mailColumn.setCellValueFactory(
+        person -> new ReadOnlyObjectWrapper<String>(person.getValue().mail()));
 
-        tableView.getColumns().add(column1);
-        tableView.getColumns().add(column2);
+    tableView.getColumns().add(nameColumn);
+    tableView.getColumns().add(mailColumn);
 
-        tableView.getItems().add(new Person("John", "Doe"));
-        tableView.getItems().add(new Person("Jane", "Deer"));
+    tableView
+        .getItems()
+        .add(
+            new Person(
+                "John Doe", Gender.MALE, "john.doe@mail.com", LocalDate.of(1973, Month.MAY, 12)));
+    tableView
+        .getItems()
+        .add(
+            new Person(
+                "Jane Deer",
+                Gender.FEMALE,
+                "jane.deer@mail.com",
+                LocalDate.of(2003, Month.OCTOBER, 2)));
+    VBox vbox = new VBox(tableView);
+    pane.setCenter(vbox);
 
-        VBox vbox = new VBox(tableView);
+    var scene = new Scene(pane);
+    stage.setScene(scene);
+    stage.show();
+  }
 
-        Scene scene = new Scene(vbox);
-
-        stage.setScene(scene);
-
-        stage.show();
-        */
-      
-
-
-    }
-
-    public static void main(String[] args) {
-        launch();
-    }
-
+  public static void main(String[] args) {
+    launch();
+  }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,6 @@
 module com.goeckeler.ulmer {
     requires javafx.controls;
   requires javafx.graphics;
+requires javafx.base;
     exports com.goeckeler.ulmer;
 }
