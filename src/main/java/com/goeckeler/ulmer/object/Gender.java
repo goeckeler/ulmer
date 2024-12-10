f9e7f33246faf7cea4b3844024af4f8d3f0d0853
Thorsten Göckeler <thorsten.goeckeler@iteratec.com>
2024-12-11 00:37:44 +0100

    Show gender in lower case

--- src/main/java/com/goeckeler/ulmer/object/Gender.java
+++ src/main/java/com/goeckeler/ulmer/object/Gender.java
@@ -1,5 +1,12 @@
 package com.goeckeler.ulmer.object;
 
 public enum Gender {
-	MALE, FEMALE, DIVERSE
+  MALE,
+  FEMALE,
+  DIVERSE;
+
+  @Override
+  public String toString() {
+	 return this.name().toLowerCase(); 
+  }
 }
